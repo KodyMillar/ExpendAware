d908fbd8373663d05f5f96741e339c3817d2ca05
Peter Chen <abc@abc.com>
2023-05-03 19:33:21 -0700

    separate category into two containers: category and budget

--- website/models.py
+++ website/models.py
@@ -2,13 +2,17 @@ from . import db
 
 class Category(db.Model):
     name = db.Column(db.String(50), primary_key=True)
-    budget = db.Column(db.Integer)
-    expenses = db.relationship('Expense', backref='category')
+    budgets = db.relationship('Budget')
+    expenses = db.relationship('Expense')
 
+
+class Budget(db.Model):
+    name = db.Column(db.String(50), primary_key=True)
+    amount = db.Column(db.Integer)
+    category_name = db.Column(db.String(50), db.ForeignKey('category.name'))
 
 class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     descr = db.Column(db.String(100))
     price = db.Column(db.Integer)
     category_name = db.Column(db.String(50), db.ForeignKey('category.name'))
-
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, redirect, url_for, render_template
-from .models import Expense, Category
+from .models import Expense, Category, Budget
 from . import db
 
 views = Blueprint('views', __name__)
@@ -10,22 +10,32 @@ def home():
         # -----------------------------------
         # Gets the expense from the HTML
         # -----------------------------------
-        n_descr = request.form.get('descr') 
-        n_price = request.form.get('price') 
-        n_category = request.form.get('name')
-        n_category_exp = request.form.get('category_name')
-        n_budget = request.form.get('budget')
+        category_name = request.form.get('category_name')
+
+        budget_name = request.form.get('budget_name')
+        budget_amount = request.form.get('budget_amount')
+        budget_fk_category = request.form.get('budget_fk_category')
+
+
+        exp_descr = request.form.get('exp_descr') 
+        exp_price = request.form.get('exp_price') 
+        exp_fk_category = request.form.get('exp_fk_category')
 
         # -----------------------------------
         # Provides the schema for the expense
         # -----------------------------------
-        if n_descr and n_category_exp and n_price:
-            new_expense = Expense(descr=n_descr, price=n_price, category_name=n_category_exp)
+        if category_name:
+            new_category = Category(name=category_name)
+            db.session.add(new_category)
+
+        if budget_name and budget_amount and budget_fk_category:
+            new_budget = Budget(name=budget_name, amount=budget_amount, category_name=budget_fk_category)
+            db.session.add(new_budget)
+        
+        if exp_descr and exp_price and exp_fk_category:
+            new_expense = Expense(descr=exp_descr, price=exp_price, category_name=exp_fk_category)
             db.session.add(new_expense)
         
-        if n_category and n_budget:
-            new_category = Category(name=n_category, budget=n_budget)
-            db.session.add(new_category)    
         
         # -----------------------------------
         # Adds new expense to the database
@@ -37,7 +47,8 @@ def home():
     # -----------------------------------
     # Extracts data from database
     # -----------------------------------
-    expenses = Expense.query.all()
     categories = Category.query.all()
+    budgets = Budget.query.all()
+    expenses = Expense.query.all()
 
-    return render_template("home.html", expenses=expenses, categories=categories)
+    return render_template("home.html", categories=categories, budgets=budgets, expenses=expenses)
