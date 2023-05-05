e168fa52f317fc1bdf94fd3e4124afc7a74db5ca
Kody <kodymillar@hotmail.com>
2023-05-04 19:37:31 -0700

    Create categories page list

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask, request, jsonify, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/categories')
+def categories():    
+    return render_template('categories.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
