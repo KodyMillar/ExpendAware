4df4a8736bdb0f2230a6cf13508c0de51edda4aa
Kody <kodymillar@hotmail.com>
2023-05-07 12:37:58 -0700

    added box shadow after clicking category

--- static/script.js
+++ static/script.js
@@ -1,3 +1,6 @@
+/* side bar menu
+-------------------------------------------*/
+
 closeButton = document.querySelector("#side-menu-close")
 sideMenu = document.querySelector("#menu-expand")
 menuButton = document.querySelector(".menu")
@@ -16,3 +19,33 @@ function closeMenu() {
 }
 
 closeButton.addEventListener("click", closeMenu)
+
+
+/* categories page 
+-------------------------------------------*/
+
+const categoriesList = document.querySelectorAll(".category")
+let categoryIndex = 0;
+let compareIndex = 0;
+
+for(let category of categoriesList){
+    category.addEventListener("click", () => {
+        category.classList.toggle("category-clicked");
+        let compareIndex = 0;
+        // for(let category of categoriesList) {
+        //     // console.log(compareIndex);
+        //     // console.log(categoryIndex)
+        //     if (category.classList.contains("category-clicked") && compareIndex !== categoryIndex)
+        //         {
+        //             category.classList.toggle("category-clicked");
+        //         }
+        //     compareIndex += 1;
+        // }
+        // categoryIndex += 1;
+        // console.log(categoryIndex);
+        // if (categoryIndex === categoriesList.length-1) {
+        //     categoryIndex = 0;
+        //     console.log(categoryIndex)
+        // }
+    });
+}
