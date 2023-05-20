48d8d3b35203f552e5619abbe898c7d45c640489
Kody <kodymillar@hotmail.com>
2023-05-19 17:03:03 -0700

    change color of alert message

--- static/javascript/home.js
+++ static/javascript/home.js
@@ -68,8 +68,8 @@ const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 const appendAlert = (message, type) => {
     const wrapper = document.createElement('div')
     wrapper.innerHTML = 
-        `<div class="alert alert-primary d-flex align-items-center" role="alert" data-bs-dismiss="alert" id="alert-box">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
+        `<div class="alert alert-warning d-flex align-items-center alert-dismissible fade show" role="alert" data-bs-dismiss="alert" id="alert-box">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                 <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>
             </svg>
@@ -150,7 +150,7 @@ addExpenseForm.onsubmit = function(e) {
                 expense_total += parseInt(expense.textContent);
             }
             if ((parseInt(addExpenseForm.amount.value) + expense_total) > parseInt(budget.querySelector(".totals-budget-amount").textContent)) {
-                appendAlert("Cannot spend more than budget limit");
+                appendAlert("You've reached your budget limit!");
                 e.preventDefault();
             }
         }
